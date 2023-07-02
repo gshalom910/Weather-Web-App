a424d34a2399cdaafe9b7243df7684a0ccaeb744
gshalom910 <91600692+gshalom910@users.noreply.github.com>
2023-07-02 18:24:54 +0300

    initial commit

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,33 +3,6 @@ import axios from 'axios';
 import "./search.css";
 
 function SearchForm() {
-  // let [city, setCity] = useState(null);
-  // let [loaded, setLoaded] = useState(false);
-  // let [weather, setWeather] = useState({});
-  // function showCity(response) {
-  //   setLoaded(true);
-  //   setWeather({
-  //     celiTemp: Math.round(response.data.main.temp),
-  //     humidity: response.data.main.humidity,
-  //     desc: response.data.weather[0].description,
-  //     wind: response.data.wind.speed,
-  //     icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  //   });
-  //   // console.log(response.data);
-  // }
-
-  // function handleForm(event) {
-  //   event.preventDefault();
-  //   if (!city) {
-  //     return;
-  //   } else {
-  //     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=616b14cbd38253313b3b8852fa77335d&units=metric`;
-  //     axios.get(url).then(showCity);
-  //   }
-  // }
-  // function updateCity(event) {
-  //   setCity(event.target.value);
-  // }
   return (
     <form id="searchForm">
       <div className="input-group pb-5 py-md-0 px-2 px-md-5">
